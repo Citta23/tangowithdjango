b4dde350b37423502e1bc974013657ebdfa6cca2
Pregashni Mohanarajah <cittapregashni@btinternet.com>
2015-01-18 20:10:48 +0000

    Chapter 4 - completed

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Rango says hey there world!<br/> <a href='/rango/about'>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.<br/> This tutorial has been put together by Pregashni Mohanarajah, 2083114<br/> <a href='/rango/'>Index</a>")
