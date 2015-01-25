c5f8be49d4052932a47904df9b3094b93029878c
Pregashni Mohanarajah <cittapregashni@btinternet.com>
2015-01-25 17:58:04 +0000

    Chapter 7 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    return render(request, 'rango/index.html', context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    return render(request, 'rango/index.html', {'categories': category_list, 'pages': page_list})
 
 
 def about(request):
     context_dict = {'author': "This tutorial has been put together by Pregashni Mohanarajah, 2083114"}
     return render(request, 'rango/about.html', context_dict)
 
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
