5dcf0b738d37ad5af7f8f00f58333a0b989aa7e6
Pregashni Mohanarajah <cittapregashni@btinternet.com>
2015-02-26 22:17:02 +0000

    Chapter 11 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
@@ -9,11 +10,42 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    return render(request, 'rango/index.html', {'categories': category_list, 'pages': page_list})
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            visits += 1
+            reset_last_visit_time = True
+
+    else:
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
 
 
 def about(request):
-    context_dict = {'author': "This tutorial has been put together by Pregashni Mohanarajah, 2083114"}
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+    context_dict = {'author': "This tutorial has been put together by Pregashni Mohanarajah, 2083114",
+                    'visits': count}
+
     return render(request, 'rango/about.html', context_dict)
 
 
@@ -114,7 +146,7 @@ def user_login(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html',
-        {'message': "Since you're logged in, you can see this text!"})
+                  {'message': "Since you're logged in, you can see this text!"})
 
 
 @login_required
